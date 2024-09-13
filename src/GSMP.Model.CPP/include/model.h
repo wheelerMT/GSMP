7000819a8183f7e007fcc728793fb05be07a2876
Matt Wheeler <wheelermt@proton.me>
2024-09-13 17:05:49 +0100

    Add Model class with GSMP variable management
    Introduced a new `Model` class for managing GSMP variables. The class includes methods to add, initialize, reinitialize, step, and terminate variables. Helper function `createVariable` was also added to support variable creation with specific types and intents.

--- src/GSMP.Model.CPP/include/model.h
+++ src/GSMP.Model.CPP/include/model.h
@@ -1,8 +1,37 @@
-//
-// Created by Matthew Wheeler on 13/09/2024.
-//
-
 #ifndef MODEL_H
 #define MODEL_H
+#include <list>
+#include "GSMP_model_types.h"
 
+class Model {
+public:
+    std::list<GSMP_Variable> variables;
+
+    Model();
+
+    ~Model();
+
+    /* ┌────────────────────────────────────────────────────────────────┐ */
+    /* │                      Model Backend Setup                       │ */
+    /* └────────────────────────────────────────────────────────────────┘ */
+
+    // Add a variable to the model list
+    void AddVariable(const GSMP_Variable &variable);
+
+    /* ┌────────────────────────────────────────────────────────────────┐ */
+    /* │                      Model Runtime                             │ */
+    /* └────────────────────────────────────────────────────────────────┘ */
+
+    void initialise();
+
+    void reinitialise();
+
+    void step() const;
+
+    void terminate();
+
+private:
+    // Helper function for creating a variable
+    static GSMP_Variable createVariable(GSMP_VariableType type, GSMP_VariableIntent intent);
+};
 #endif //MODEL_H
