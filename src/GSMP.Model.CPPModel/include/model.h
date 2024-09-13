33703cbd358ff6159fdb6617ab85a10d08dd6348
Matt Wheeler <wheelermt@proton.me>
2024-09-13 10:43:24 +0100

    Add basic C++ project structure

---
+++ src/GSMP.Model.CPPModel/include/model.h
@@ -0,0 +1,8 @@
+//
+// Created by Matthew Wheeler on 13/09/2024.
+//
+
+#ifndef MODEL_H
+#define MODEL_H
+
+#endif //MODEL_H
---
+++ src/GSMP.Model.CPPModel/src/model.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Matthew Wheeler on 13/09/2024.
+//
+
+#include "model.h"
