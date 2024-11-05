52ed1ccf98f62daacd55168d9b86f1efb8e9b1f1
Matt Wheeler <wheelermt@proton.me>
2024-11-05 21:07:04 +0000

    Update `Model` functionality.
    Removed initialisation and step functionality for now. All model variables are now encapsulated in an `unordered_map`, where each key is the variable name.

--- src/models/CPP/src/model.cpp
+++ src/models/CPP/src/model.cpp
@@ -1,95 +1,59 @@
 #include "model.h"
-#include "model_types.h"
+#include <format>
+#include <stdexcept>
+#include "model_variable.h"
 
 Model::Model() { initialise(); }
 
 Model::~Model() { terminate(); }
 
-void Model::add_variable(const ModelVariable &variable) { variables.push_back(variable); }
+void Model::add_variable(const ModelVariable &variable)
+{
+    auto [it, success] = _variables.emplace(variable.name(), variable);
+
+    if (!success)
+    {
+        throw std::runtime_error(std::format("Variable '{}' already exists", it->first));
+    }
+}
 
 void Model::initialise()
 {
-    // Create a set of inputs
-    for (int i = 0; i < 3; ++i)
-    {
-        variables.push_back(create_variable(static_cast<ModelVariableType>(i), ModelVariableIntent::Input));
-    }
-
-    // Create a set of outputs
-    for (int i = 0; i < 3; ++i)
-    {
-        variables.push_back(create_variable(static_cast<ModelVariableType>(i), ModelVariableIntent::Output));
-    }
+    // Initialise all model variables
 }
 
 void Model::reinitialise()
 {
-    variables.clear();
+    _variables.clear();
     initialise();
 }
 
-void Model::step() const
+void Model::step()
 {
-    for (auto variable: variables)
-    {
-        if (variable.intent == ModelVariableIntent::Output)
-        {
-            switch (variable.type)
-            {
-                case ModelVariableType::Int:
-                    variable.value = std::get<int>(variable.value) + 1;
-                    break;
-                case ModelVariableType::Float:
-                    variable.value = std::get<float>(variable.value) + 1.0f;
-                    break;
-                case ModelVariableType::Double:
-                    variable.value = std::get<double>(variable.value) + 1.0;
-                    break;
-            }
-        }
-    }
+    // Step the model and update variables accordingly
 }
 
-void Model::terminate() { variables.clear(); }
+void Model::terminate() { _variables.clear(); }
 
-ModelVariable Model::create_variable(ModelVariableType type, ModelVariableIntent intent)
+std::list<ModelVariable> Model::variables() const
 {
-    // Determine default value based on type
-    std::string name;
-    std::variant<int, float, double> value;
-    switch (type)
+    std::list<ModelVariable> result;
+
+    for (const auto &[name, variable]: _variables)
     {
-        case ModelVariableType::Int:
-            value = 1;
-            name = "int";
-            break;
-        case ModelVariableType::Float:
-            value = 1.0f;
-            name = "float";
-            break;
-        case ModelVariableType::Double:
-            value = 1.0;
-            name = "double";
-            break;
-        default:
-            value = 1;
+        result.emplace_back(variable);
     }
 
-    // Determine prefix based on intent
-    std::string prefix;
-    switch (intent)
-    {
-        case ModelVariableIntent::Input:
-            prefix = "i_";
-            break;
-        case ModelVariableIntent::Output:
-            prefix = "o_";
-            break;
-        default:
-            prefix = "undef_";
-    }
-    name = prefix + name;
+    return result;
+}
 
-    // Construct GSMP_VariableType
-    return {name, type, value, intent};
+ModelVariable Model::get_variable(const std::string &name) const
+{
+    try
+    {
+        return _variables.at(name);
+    } catch (const std::out_of_range &)
+    {
+        throw std::runtime_error(std::format("Variable '{}' does not exist within list of model variables", name));
+    }
 }
