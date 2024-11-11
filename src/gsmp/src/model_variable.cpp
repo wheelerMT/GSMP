dfe690c58c64022df08911b3ca53137397f7ec9c
Matt Wheeler <wheelermt@proton.me>
2024-11-11 21:03:55 +0000

    Update set_value and get_value to use GSMPType
    These methods are now much more robust, and don't rely on awkward templating in order to work.

--- src/gsmp/src/model_variable.cpp
+++ src/gsmp/src/model_variable.cpp
@@ -1,47 +1,42 @@
 #include "model_variable.h"
-#include <format>
 #include <stdexcept>
 
-template<typename T>
-T ModelVariable::get_value() const
+void ModelVariable::set_value(const GSMPType &new_value)
 {
-    static_assert(std::is_same_v<T, int> || std::is_same_v<T, float> || std::is_same_v<T, double>,
-                  "Can only retrieve int, float, or double types.");
-
-    try
-    {
-        return std::get<T>(_value);
-    } catch (const std::bad_variant_access &)
-    {
-        throw std::runtime_error(
-                std::format("Stored type does not match the requested type. Stored type index: {}", _value.index()));
-    }
-}
-
-template<typename T>
-void ModelVariable::set_value(T new_value)
-{
-    static_assert(std::is_same_v<T, int> || std::is_same_v<T, float> || std::is_same_v<T, double>,
-                  "Can only set int, float, or double types.");
-    _value = new_value;
+    // Ensures new value matches the current type
+    std::visit(
+            [this](const auto &val)
+            {
+                using NewType = std::decay_t<decltype(val)>;
+                if (!std::holds_alternative<NewType>(_value))
+                {
+                    throw std::runtime_error("Type mismatch in set_value");
+                }
+                _value = val;
+            },
+            new_value);
 }
 
 std::string ModelVariable::get_type_string() const
 {
-    if (std::holds_alternative<int>(_value))
-        return "int";
-    if (std::holds_alternative<float>(_value))
-        return "float";
-    if (std::holds_alternative<double>(_value))
-        return "double";
-
-    return "unknown";
+    return std::visit(
+            [](const auto &arg) -> std::string
+            {
+                using T = std::decay_t<decltype(arg)>; // Decays "const int&" to just "int", etc.
+                if constexpr (std::is_same_v<T, int>)
+                    return "int";
+                else if constexpr (std::is_same_v<T, float>)
+                    return "float";
+                else if constexpr (std::is_same_v<T, double>)
+                    return "double";
+                else
+                    return "unknown";
+            },
+            _value);
 }
 
 template<typename T>
 T ModelVariable::as() const
 {
-    static_assert(std::is_same_v<T, int> || std::is_same_v<T, float> || std::is_same_v<T, double>,
-                  "Can only convert to int, float, or double types.");
     return std::visit([](auto &&arg) -> T { return static_cast<T>(arg); }, _value);
 }
