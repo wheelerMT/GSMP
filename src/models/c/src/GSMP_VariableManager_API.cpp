9c2a627a37474487647450fe68c1b4f9ee4eb6f2
mckenziebrb <mckenziebrb@gmail.com>
2024-12-03 18:50:42 +0000

    Add prototype for GSMP_VariableManager_AddVariable. Add TODO for GSMP_VariableManager_Create/Destroy. Refer to Issue GSMP-11. Update arguments of prototyped declarations/definitions. Assumes: > *<type> for input instance/opaque pointer. > const <type> for inputs. > *<type> for outputs. Added prefixing 'p' for pointers.

--- src/models/c/src/GSMP_VariableManager_API.cpp
+++ src/models/c/src/GSMP_VariableManager_API.cpp
@@ -18,6 +18,8 @@
 // Model Instance Management
 GSMP_API GSMP_VariableManager_Create(GSMP_Instance *instance)
 {
+    // TODO How to handle NULL vs nullptr https://devblogs.microsoft.com/oldnewthing/20180307-00/?p=98175
+    // TODO Utility:ErrorNot_NULL/ErrorNot_nullptr
     if (instance != nullptr)
     {
         std::cerr << "[ERROR] Non-null pointer passed to GSMP_VariableManager_Create. Instance already initialized.\n";
@@ -29,16 +31,18 @@ GSMP_API GSMP_VariableManager_Create(GSMP_Instance *instance)
         instance = reinterpret_cast<GSMP_Instance *>(new VariableManager());
     } catch (const std::bad_alloc &)
     {
+        // TODO Utility:ErrorMessage e.g. int ErrorMessage(returnCode, const char*) - which always returns 'ERROR' or a
+        // more specific 'ERROR' code.
         std::cerr << "[ERROR] Memory allocation failed in GSMP_VariableManager_Create.\n";
         return ERROR; // Memory allocation failure
     } catch (const std::exception &ex)
     {
         std::cerr << "[ERROR] Exception caught in GSMP_VariableManager_Create: " << ex.what() << '\n';
-        return ERROR; // Other standard exceptions
+        return ERROR;
     } catch (...)
     {
-        std::cerr << "[INFO] VariableManager instance successfully created.\n";
-        return ERROR; // Catch-all for non-standard exceptions
+        std::cerr << "[ERROR] Failed in GSMP_VariableManager_Create.\n";
+        return ERROR;
     }
 
     return OK;
@@ -55,12 +59,12 @@ GSMP_API GSMP_VariableManager_Destroy(GSMP_Instance *instance)
 
     try
     {
-        std::cerr << "[DEBUG] Deleting VariableManager instance.\n";
         delete reinterpret_cast<VariableManager *>(instance);
-        instance = nullptr; // Set the pointer to null to avoid dangling pointers
+        instance = nullptr; // TODO NULL vs nullptr
     } catch (const std::exception &ex)
     {
-        std::cerr << "[ERROR] Exception caught while deleting instance: " << ex.what() << '\n';
+        std::cerr << "[ERROR] Exception caught in GSMP_VariableManager_Destroy while deleting instance: " << ex.what()
+                  << '\n';
         return ERROR;
     } catch (...)
     {
@@ -73,39 +77,53 @@ GSMP_API GSMP_VariableManager_Destroy(GSMP_Instance *instance)
 }
 
 // Model Variable Instantiation
-// QUESTION - C to CPP types.
-// QUESTION - During Instantiation, we need to copy data when instantiating new instances of objects???
-// QUESTION - When runtime/querying we wish to return current state as reference or as copy ???
-GSMP_API GSMP_VariableManager_AddVariable_Int(GSMP_Instance *instance, const string_t name, int_t *value,
-                                              VariableIntent *intent, string_t description)
+GSMP_API GSMP_VariableManager_AddVariable_Int(GSMP_Instance *instance, const string_t name, const int_t value,
+                                              VariableIntent intent, const string_t description)
 {
     if (instance = nullptr)
     {
-        std::cerr << "[ERROR] Null pointer passed to GSMP_VariableManager_Destroy. Cannot destroy uninitialized "
-                     "instance.\n";
+        std::cerr << "[ERROR] Null pointer passed to GSMP_VariableManager_AddVariable_Int. Cannot destroy "
+                     "uninitialized instance.\n";
         return ERROR; // Instance not initialized
     }
-
-    // TODO Check for invalid arguments: name, intent, value, description.
-    // if ()
-    // {
-    //     std::cerr << "[ERROR] Null name provided to GSMP_VariableManager_AddVariable_Int.\n";
-    //     return ERROR;
-    // }
+    // TODO Check for invalid arguments: name, intent, value, description, or just catch associated exceptions.
 
     try
     {
         // Cast to actual implementation class
         VariableManager *variableManager = reinterpret_cast<VariableManager *>(instance);
 
-        // TODO Cast/Intermediary Types (C to Cpp)
+        // Intermediary Types (C to Cpp)
+        std::string cppName = name;
+        std::string cppDescription = description;
+
+        // ISSUE: GSMP-17 - Require intent to be compatible.
+        ModelVariableIntent cppIntent = ModelVariableIntent::Undefined;
+        switch (intent)
+        {
+            case VariableIntent::INPUT:
+                cppIntent = ModelVariableIntent::Input;
+                break;
+            case VariableIntent::OUTPUT:
+                cppIntent = ModelVariableIntent::Output;
+                break;
+            case VariableIntent::OVERRIDE:
+                cppIntent = ModelVariableIntent::Override;
+                break;
+            default:
+                std::cerr
+                        << "[ERROR] Null pointer passed to GSMP_VariableManager_Destroy. Cannot destroy uninitialized "
+                           "instance.\n";
+                break;
+        }
+        ModelVariableIntent cppIntent = ModelVariableIntent::Input;
 
         // Create Variable
-        // ModelVariable modelVariable = new ModelVariable(name, value, intent, description);
+        // ISSUE: GSMP-17 : Check constructor assignment shallow or deep copy.
+        ModelVariable *modelVariable = new ModelVariable(cppName, value, cppIntent, cppDescription);
 
         // Add Variable to VariableManager instance
-        std::cerr << "[DEBUG] Calling VariableManager::add_variable.\n";
-        ReturnCode returnCode = variableManager->add_variable(modelVariable);
+        variableManager->add_variable(*modelVariable);
 
     } catch (const std::exception &ex)
     {
@@ -120,22 +138,24 @@ GSMP_API GSMP_VariableManager_AddVariable_Int(GSMP_Instance *instance, const str
     std::cerr << "[INFO] GSMP_VariableManager_AddVariable_Int completed successfully.\n";
     return OK;
 }
-GSMP_API GSMP_VariableManager_AddVariable_Single(GSMP_Instance *instance, string_t name, int_t *value,
-                                                 VariableIntent *intent, string_t description);
-GSMP_API GSMP_VariableManager_AddVariable_Double(GSMP_Instance *instance, string_t name, int_t *value,
-                                                 VariableIntent *intent, string_t description);
+GSMP_API GSMP_VariableManager_AddVariable_Single(GSMP_Instance *instance, const string_t name, const single_t value,
+                                                 VariableIntent intent, const string_t description);
+GSMP_API GSMP_VariableManager_AddVariable_Double(GSMP_Instance *instance, const string_t name, const double_t value,
+                                                 VariableIntent intent, const string_t description);
 
 // Model Variables (Runtime)
-GSMP_API GSMP_VariableManager_GetVariableType(GSMP_Instance *instance, string_t name, int_t *value);
+GSMP_API GSMP_VariableManager_GetVariableType(GSMP_Instance *instance, const string_t name, int_t *pValue);
 
-GSMP_API GSMP_VariableManager_GetVariableIntent(GSMP_Instance *instance, string_t name, VariableIntent *variableIntent);
+GSMP_API GSMP_VariableManager_GetVariableIntent(GSMP_Instance *instance, const string_t name,
+                                                VariableIntent *pVariableIntent);
 
-GSMP_API GSMP_VariableManager_GetVariableDescription(GSMP_Instance *instance, string_t name, int_t *value);
+GSMP_API GSMP_VariableManager_GetVariableDescription(GSMP_Instance *instance, const string_t name,
+                                                     string_t *pDescription);
 
-GSMP_API GSMP_VariableManager_SetVariable_Int(GSMP_Instance *instance, string_t name, int_t *value);
-GSMP_API GSMP_VariableManager_SetVariable_Single(GSMP_Instance *instance, string_t name, single_t *value);
-GSMP_API GSMP_VariableManager_SetVariable_Double(GSMP_Instance *instance, string_t name, double_t *value);
+GSMP_API GSMP_VariableManager_SetVariable_Int(GSMP_Instance *instance, const string_t name, int_t *pValue);
+GSMP_API GSMP_VariableManager_SetVariable_Single(GSMP_Instance *instance, const string_t name, single_t *pValue);
+GSMP_API GSMP_VariableManager_SetVariable_Double(GSMP_Instance *instance, const string_t name, double_t *pValue);
 
-GSMP_API GSMP_VariableManager_GetVariable_Int(GSMP_Instance *instance, string_t name, int_t *value);
-GSMP_API GSMP_VariableManager_GetVariable_Single(GSMP_Instance *instance, string_t name, single_t *value);
-GSMP_API GSMP_VariableManager_GetVariable_Double(GSMP_Instance *instance, string_t name, double_t *value);
+GSMP_API GSMP_VariableManager_GetVariable_Int(GSMP_Instance *instance, const string_t name, int_t *pValue);
+GSMP_API GSMP_VariableManager_GetVariable_Single(GSMP_Instance *instance, const string_t name, single_t *pValue);
+GSMP_API GSMP_VariableManager_GetVariable_Double(GSMP_Instance *instance, const string_t name, double_t *pValue);
